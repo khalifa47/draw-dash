27901601082a999761a356b231695e2d4109685b
khalifa47 <khalifafumo5@gmail.com>
2024-08-16 18:27:22 +0300

    Revert "Revert "fix: move image comparison to client side using threads""
    This reverts commit 4fd8fa101e89ca66d93f06aa6c543f04f25d93ea.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+  webpack: (config) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "sharp$": false,
+      "onnxruntime-node$": false,
+    }
+    return config;
   },
   images: {
     remotePatterns: [
