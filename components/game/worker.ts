5366a1557d7036e6dc2066dca537cf149e6d4e56
khalifa47 <khalifafumo5@gmail.com>
2024-08-16 15:19:56 +0300

    Revert "fix: remove resizing"
    This reverts commit d25fad998d648db1340b23d2c87dfa5a56d08743.

--- components/game/worker.ts
+++ components/game/worker.ts
@@ -12,7 +12,8 @@ env.allowLocalModels = false;
 
 const cosineSimilarity = (
   query_embeds: Float32Array,
-  ans_embeds: Float32Array
+  ans_embeds: Float32Array,
+  chunkSize: number
 ) => {
   if (query_embeds.length !== ans_embeds.length) {
     throw new Error("Embeddings must be of the same length");
@@ -22,13 +23,16 @@ const cosineSimilarity = (
   let normQuery = 0;
   let normAns = 0;
 
-  for (let i = 0; i < query_embeds.length; ++i) {
-    const queryValue = query_embeds[i];
-    const dbValue = ans_embeds[i];
+  for (let i = 0; i < query_embeds.length; i += chunkSize) {
+    const end = Math.min(i + chunkSize, query_embeds.length);
+    for (let j = i; j < end; ++j) {
+      const queryValue = query_embeds[j];
+      const dbValue = ans_embeds[j];
 
-    dotProduct += queryValue * dbValue;
-    normQuery += queryValue * queryValue;
-    normAns += dbValue * dbValue;
+      dotProduct += queryValue * dbValue;
+      normQuery += queryValue * queryValue;
+      normAns += dbValue * dbValue;
+    }
   }
 
   return dotProduct / (Math.sqrt(normQuery) * Math.sqrt(normAns));
@@ -67,10 +71,16 @@ self.addEventListener("message", async (event) => {
 
   const { query_image, ans_image } = event.data;
 
-  // Read the two images as raw images
-  const rawImageQuery = await RawImage.fromURL(query_image);
+  console.log("query_image", query_image);
+  console.log("ans_image", ans_image);
 
-  const rawImageAns = await RawImage.fromURL(ans_image);
+  // Read the two images as raw images
+  const rawImageQuery = await (
+    await RawImage.fromURL(query_image)
+  ).resize(256, 256);
+  const rawImageAns = await (
+    await RawImage.fromURL(ans_image)
+  ).resize(256, 256);
 
   // Tokenize the two images
   const tokenizedImageQuery = await processor(rawImageQuery);
@@ -88,7 +98,7 @@ self.addEventListener("message", async (event) => {
     Object.values(embedsAns.data) as number[]
   );
 
-  const similarity = cosineSimilarity(queryEmbedsArray, ansEmbedsArray);
+  const similarity = cosineSimilarity(queryEmbedsArray, ansEmbedsArray, 128);
 
   // Send the output back to the main thread
   self.postMessage({
