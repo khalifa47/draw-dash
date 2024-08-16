06fcd7aeab21eb1ff74d8d368843faa638126fab
khalifa47 <khalifafumo5@gmail.com>
2024-08-16 15:20:19 +0300

    Revert "fix: chunk processing"
    This reverts commit 70c8829b6228184dcf8d64878379008a158bc003.

--- app/actions/compare.ts
+++ app/actions/compare.ts
@@ -9,8 +9,7 @@ import {
 
 const cosineSimilarity = (
   query_embeds: Float32Array,
-  ans_embeds: Float32Array,
-  chunkSize: number
+  ans_embeds: Float32Array
 ) => {
   if (query_embeds.length !== ans_embeds.length) {
     throw new Error("Embeddings must be of the same length");
@@ -20,19 +19,17 @@ const cosineSimilarity = (
   let normQuery = 0;
   let normAns = 0;
 
-  for (let i = 0; i < query_embeds.length; i += chunkSize) {
-    const end = Math.min(i + chunkSize, query_embeds.length);
-    for (let j = i; j < end; ++j) {
-      const queryValue = query_embeds[j];
-      const dbValue = ans_embeds[j];
+  for (let i = 0; i < query_embeds.length; ++i) {
+    const queryValue = query_embeds[i];
+    const dbValue = ans_embeds[i];
 
-      dotProduct += queryValue * dbValue;
-      normQuery += queryValue * queryValue;
-      normAns += dbValue * dbValue;
-    }
+    dotProduct += queryValue * dbValue;
+    normQuery += queryValue * queryValue;
+    normAns += dbValue * dbValue;
   }
 
-  return dotProduct / (Math.sqrt(normQuery) * Math.sqrt(normAns));
+  const similarity = dotProduct / (Math.sqrt(normQuery) * Math.sqrt(normAns));
+  return similarity;
 };
 
 export async function compare(query_image: string, ans_image: string) {
@@ -42,10 +39,10 @@ export async function compare(query_image: string, ans_image: string) {
   // Read the two images as raw images
   const rawImageQuery = await (
     await RawImage.fromURL(query_image)
-  ).resize(256, 256);
+  ).resize(512, 512);
   const rawImageAns = await (
     await RawImage.fromURL(ans_image)
-  ).resize(256, 256);
+  ).resize(512, 512);
 
   // Tokenize the two images
   const tokenizedImageQuery = await processor(rawImageQuery);
@@ -63,7 +60,7 @@ export async function compare(query_image: string, ans_image: string) {
     Object.values(embedsAns.data) as number[]
   );
 
-  const similarity = cosineSimilarity(queryEmbedsArray, ansEmbedsArray, 128);
+  const similarity = cosineSimilarity(queryEmbedsArray, ansEmbedsArray);
 
   return similarity;
 }
